55dd275bd83b6aa8d4a768cfdee53a0bf4ac804f
Audry <audry@greyedsolutions.com>
2022-05-03 15:18:48 -0500

    un-indented code block

--- src/App.vue
+++ src/App.vue
@@ -19,14 +19,14 @@
         padding: 30px;
     }
 
-        nav a {
-            font-weight: bold;
-            color: #2c3e50;
-        }
+    nav a {
+        font-weight: bold;
+        color: #2c3e50;
+    }
 
-            nav a.router-link-exact-active {
-                color: #42b983;
-            }
+    nav a.router-link-exact-active {
+        color: #42b983;
+    }
 
     h4 {
         font-size: 20px;
