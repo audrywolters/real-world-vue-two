04e7def75e240ed173f3fbed7d4f58e80e9b6a93
Audry <audry@greyedsolutions.com>
2022-05-03 12:52:46 -0500

    added scoped and global styles to card and h1. added a header to home page too.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="home">
-    <EventCard v-for="event in events" :key="event.id" :event="event" />
-  </div>
+    <h1>Events for Good</h1>
+    <div class="events">
+        <EventCard v-for="event in events" :key="event.id" :event="event" />
+    </div>
 </template>
 
 <script>
@@ -55,3 +56,11 @@ export default {
 
 }
 </script>
+
+<style scoped>
+.events {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+</style>
