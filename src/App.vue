58129199cfa0c34a862771474284c98fadcde9d0
Audry <audry@greyedsolutions.com>
2022-05-04 10:03:11 -0500

    router cleanup stuff

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
     <nav>
-        <router-link to="/">Events</router-link> |
+        <router-link :to="{ name: 'EventList' }">Events</router-link> |
         <router-link to="/about">About</router-link>
+        <!-- :to="{ name: 'About' }" does not work -->
     </nav>
     <router-view />
 </template>
